36b50d24032d559ebb246a5dc58983f3140aa4c7
practicalcomputerscience <apfelmus@my.mail.de>
2025-11-07 17:27:49 +0100

    Add files via upload
    changed back single line comment char % to Prolog block commenting with /* ... */, like in the other Prolog source code

--- 03 - source code/04 - logic programming/Prolog/graph_4coloring_Germany2b_Ciao.pl
+++ 03 - source code/04 - logic programming/Prolog/graph_4coloring_Germany2b_Ciao.pl
@@ -1,23 +1,27 @@
-% graph_4coloring_Germany2b_Ciao.pl
-%
-%  source: https://bernardopires.com/2013/10/try-logic-programming-a-gentle-introduction-to-prolog/
-%
-%  test on Ciao Prolog Ciao 1.25.0 [LINUXx86_64], 2025-11-06: OK!!!
-%
-%  build program in Ubuntu 24 LTS: $ ciao comp -S ./graph_4coloring_Germany2b_Ciao.pl  # -S for standalone executable
-%
-%  run program in Ubuntu 24 LTS:   $ ./graph_4coloring_Germany2b_Ciao
-%                                  $ time ./graph_4coloring_Germany2b_Ciao => real	0m0,770s <<<!!
-%
-%
-%
-%  $ xxx  # version info??
-%
-%
+/* graph_4coloring_Germany2b_Ciao.pl
+
+   source: https://bernardopires.com/2013/10/try-logic-programming-a-gentle-introduction-to-prolog/
+
+   test on Ciao Prolog Ciao 1.25.0 [LINUXx86_64], 2025-11-07: OK
+
+   build program in Ubuntu 24 LTS: $ ciao comp -S ./graph_4coloring_Germany2b_Ciao.pl  # -S for standalone executable
+
+   run program in Ubuntu 24 LTS:   $ ./graph_4coloring_Germany2b_Ciao
+                                   $ time ./graph_4coloring_Germany2b_Ciao => real	0m0,770s <<<!!
+
+
+
+   $ ciao
+   Ciao 1.25.0 [LINUXx86_64]
+   ?- halt.
+   $ 
+
+*/
+
 
 :- initialization(main).
 
-:- use_module(library(lists)).  % this is different from GNU and SWI Prolog
+:- use_module(library(lists)).  /* this is different from GNU and SWI Prolog */
 
 color(red).
 color(green).
@@ -25,7 +29,7 @@ color(blue).
 color(yellow).
 
 neighbor(StateAColor, StateBColor) :- color(StateAColor), color(StateBColor), StateAColor \= StateBColor.
-% \= is the negated explicit unification operator
+/* \= is the negated explicit unification operator */
 
 germany( SH, MV, HH, HB, NI, ST, BE, BB, SN, NW, HE, TH, RP, SL, BW, BY ) :-
 neighbor(SH, NI),
@@ -65,7 +69,7 @@ main :- findall( (SH, MV, HH, HB, NI, ST, BE, BB, SN, NW, HE, TH, RP, SL, BW, BY
                       length(L, X),
                       write_solutions(L, X),
                       write('\n'),
-        halt(0).  % exit this program
+        halt(0).  /* exit this program */
 
 write_solutions(L, X) :- write('number N of different solutions = '), write(X), nl, write_elements(L, X).
 
@@ -73,20 +77,22 @@ write_elements(L, X)  :- write_header, nth(1, L, Elem_0), write('1st solution = 
                       Last is X - 1, nth(Last, L, Elem_N),
                       write('...'), nl,
                       write('Last solution = '), write(Elem_N).
-                      % nth() is different from GNU and SWI Prolog
+                      /* nth() is different from GNU and SWI Prolog */
 
 write_header :- nl, write('               SH, MV, HH, HB, NI, ST, BE, BB, SN, NW, HE, TH, RP, SL, BW, BY'), nl.
 
 
-% output:
-%   $ ./graph_4coloring_Germany2b_Ciao
-%   number N of different solutions = 191808
-%
-%                  SH, MV, HH, HB, NI, ST, BE, BB, SN, NW, HE, TH, RP, SL, BW, BY
-%   1st solution = red,blue,blue,red,green,blue,green,red,green,red,blue,red,green,red,red,yellow
-%   ...
-%   Last solution = yellow,green,green,yellow,blue,green,blue,yellow,blue,yellow,green,yellow,blue,green,yellow,red
-%                   the last solution is different from GNU and SWI Prolog!!
-%   $
+/* output:
+$ ./graph_4coloring_Germany2b_Ciao
+number N of different solutions = 191808
 
-% end of graph_4coloring_Germany2b_Ciao.pl
+               SH, MV, HH, HB, NI, ST, BE, BB, SN, NW, HE, TH, RP, SL, BW, BY
+1st solution = red,blue,blue,red,green,blue,green,red,green,red,blue,red,green,red,red,yellow
+...
+Last solution = yellow,green,green,yellow,blue,green,blue,yellow,blue,yellow,green,yellow,blue,green,yellow,red
+                the last solution is different from GNU and SWI Prolog!!
+
+$
+*/
+
+/* end of graph_4coloring_Germany2b_Ciao.pl */
