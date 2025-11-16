33904dcabb88d92065b9c3568e02f32c81534972
practicalcomputerscience <apfelmus@my.mail.de>
2025-11-16 23:21:17 +0100

    Add files via upload

--- 02 - execution times/mean_stddev_err_whiskers.R
+++ 02 - execution times/mean_stddev_err_whiskers.R
@@ -1,6 +1,6 @@
 # mean_stddev_err_whiskers.R
 #
-# 2025-05-31, 2025-06-10/15, 2025-09-28/29, 2025-10-20
+# 2025-05-31, 2025-06-10/15, 2025-09-28/29, 2025-10-20, 2025-11-16
 #
 # env: R version 4.5.0 (2025-04-11 ucrt) -- "How About a Twenty-Six"
 #      Platform: x86_64-w64-mingw32/x64
@@ -16,18 +16,25 @@ library(ggplot2)
 ##########################
 #
 # user switch:
-plot_type <- 0
-            # 0 = Master diagram   
+plot_type <-  4
+            # 0 = Master diagram
             # 1 = Java native languages Scala, Kotlin and Clojure and their speedup with the GraalVM
             # 2 = Tested Scheme dialects
+            # 3 = only virtual machine (VM) languages
+            # 4 = Prolog systems for the map coloring problem of Germany-benchmark
 #
 ##########################
 
 
 sec_to_ms <- 1000  # raw data is in seconds => convert to milliseconds for better presentation here
 
+if (plot_type < 4) {
 plot_title <- paste("Microbenchmark: execution speeds ('wall clock') of a
 hobby project program in different programming languages")
+} else if (plot_type == 4) {
+plot_title <- paste("Microbenchmark: execution speeds ('wall clock') of the
+map coloring problem of Germany with 16 states and 4 colors")
+}
 
 sub_title0 <- paste("using Linux perf-stat with 20 individual runs of each program (on Ubuntu 24 LTS)")
 
@@ -39,14 +46,17 @@ setwd("e:/zzz_Scripts/R/comparison plot with only mean and standard deviations")
 
 
 # dat1 class: data.frame
-if (plot_type > 1) {
-    dat1 <- read.csv("programming_languages_exe_speeds_Scheme.csv", header = T, dec = ",", sep = ";", comment.char = '%')
-  } else if (plot_type < 1) {
+if (plot_type == 0) {
     dat1 <- read.csv("programming_languages_exe_speeds.csv", header = T, dec = ",", sep = ";", comment.char = '%')
-  } else {
+  } else if (plot_type == 1) {
     dat1 <- read.csv("programming_languages_exe_speeds_GraalVM.csv", header = T, dec = ",", sep = ";", comment.char = '%')
+  } else if (plot_type == 2) {
+    dat1 <- read.csv("programming_languages_exe_speeds_Scheme.csv", header = T, dec = ",", sep = ";", comment.char = '%')
+  } else if (plot_type == 3) {
+    dat1 <- read.csv("programming_languages_exe_speeds_VM.csv", header = T, dec = ",", sep = ";", comment.char = '%')
+  } else if (plot_type == 4) {
+    dat1 <- read.csv("programming_languages_exe_speeds_Prolog.csv", header = T, dec = ",", sep = ";", comment.char = '%')
   }
-
 
 ylabel <-  paste("mean, +/- standard deviation in milliseconds")
 # xlabel <- paste("programming language")
@@ -61,8 +71,14 @@ dat1_sort
 #   language    mean   std_dev
 #   <chr>      <dbl>     <dbl>
 # 1 C        0.00790 0.0000327
-# 2 C3       0.0155  0.000109 
+# 2 C3       0.0155  0.000109
 
+
+if (plot_type < 4) {
+  y_break_max = 1100  # milliseconds
+} else if (plot_type == 4) {
+  y_break_max = 2300
+}
 
 # problem: ggplotting with x = language will sort it alphanumerically before!
 # --> solution: reorder() it according to mean BEFORE!
@@ -70,22 +86,22 @@ bar_plot1 <- ggplot(dat1_sort,
                # aes(x = language)) +
                aes(x = reorder(language,  mean*sec_to_ms),
                    y =  mean*sec_to_ms)) +
-               
+
                geom_errorbar(aes(ymin = (mean - std_dev)*sec_to_ms,
                                  ymax = (mean + std_dev)*sec_to_ms),
                                  width=.1) +
-               
+
                geom_point(aes(y = mean*sec_to_ms)) +
 
                scale_y_continuous(
-                 breaks = seq(0, 1100, 100)
+                 breaks = seq(0, y_break_max, 100)
                ) +
-               
+
                labs(title = plot_title,
                     subtitle = sub_title,
                     x = xlabel,
                     y = ylabel) +
-               
+
                theme(text = element_text(size = 12),
                      axis.text.x = element_text(angle = 90, size = 12))
 
@@ -93,6 +109,3 @@ print(bar_plot1)
 
 
 # end of mean_stddev_err_whiskers.R
-
-
-
