1c1ec9391b8d9a24c21f0a272bf587beb4f573b3
practicalcomputerscience <apfelmus@my.mail.de>
2025-12-21 14:33:12 +0100

    Add files via upload

--- 03 - source code/01 - imperative languages/Scala/random_streams_for_perf_stats.scala
+++ 03 - source code/01 - imperative languages/Scala/random_streams_for_perf_stats.scala
@@ -1,19 +1,33 @@
 /*
-random_bitstring_and_flexible_password_generator.scala
+random_streams_for_perf_stats.scala
 
-2025-04-05/06, 2025-05-05/19, 2025-06-01/02/03/18
+2025-06-03/08/09/10/18
 2025-12-21: see below
 
 build on Ubuntu 24 LTS: use sbt to make one "fat-Jar" (uberjar):
                       $ sbt new
                       take option d to start with a Scala 3 seed template
-                      set project name to: random_bitstring_and_flexible_password_generator
-                      $ cd ./random_bitstring_and_flexible_password_generator  # change to the project root dir
+                      set project name to: random_streams_for_perf_stats
+                      $ cd ./random_streams_for_perf_stats  # change to the project root dir
                       fix config files build.sbt in project root dir and plugins.sbt in ./project subdir
-                      copy source code file _random_bitstring_and_flexible_password_generator.scala_ as Main.scala into subdir _./src/main/scala/Main.scala
+                      copy source code file _random_streams_for_perf_stats.scala_ as _Main.scala into subdir ./src/main/scala/Main.scala
                       $ sbt assembly
 
-run on Ubuntu 24 LTS: $ java -jar ./target/scala-3.7.4/random_bitstring_and_flexible_password_generator-assembly-0.1.0-SNAPSHOT.jar
+run on Ubuntu 24 LTS: $ java -jar ./target/scala-3.7.4/random_streams_for_perf_stats-assembly-0.1.0-SNAPSHOT.jar
+
+exe time measurement:
+$ multitime -n 20 java -jar ./target/scala-3.7.4/random_streams_for_perf_stats-assembly-0.1.0-SNAPSHOT.jar
+
+see:
+Collections -- Performance Characteristics
+https://docs.scala-lang.org/overviews/collections-2.13/performance-characteristics.html
+
+
+---
+building a binary executable with GraalVM:
+$ $HOME/.sdkman/candidates/java/24-graal/lib/svm/bin/native-image -jar ./target/scala-3.6.4/random_streams_for_perf_stats-assembly-0.1.0-SNAPSHOT.jar
+$ ./random_streams_for_perf_stats-assembly-0.1.0-SNAPSHOT
+$ sudo perf stat -r 20 ./random_streams_for_perf_stats-assembly-0.1.0-SNAPSHOT
 
 
 ---
@@ -36,7 +50,7 @@ import java.io._          // for FileWriter etc.
 import scala.util.{Try}
 
 
-object random_bitstring_and_flexible_password_generator {
+object random_streams_for_perf_stats {
   def main(args: Array[String]): Unit = { // keep main() to keep easier track with the Java tool chain
 
     val END: Int = 62501  // 62501 for exactly 1M binary digits; val is immutable
@@ -91,6 +105,8 @@ object random_bitstring_and_flexible_password_generator {
     Try {
       val fileWriter1 = new FileWriter(new File(file_bits_x))
       fileWriter1.write(bits_x.toString)
+      // no speed up with: BufferedWriter()
+      // https://stackoverflow.com/questions/31751348/which-solution-has-better-performance-stringbuilder-or-string-interpolation-con
       fileWriter1.close()
     }.toEither match {
       case Left (ex) =>
@@ -110,82 +126,7 @@ object random_bitstring_and_flexible_password_generator {
       case Right(_) =>
         print(f"\nByte stream has been written to disk under name: $file_bits_hex\n")
     }
-
-
-
-    // make a password of N_CHAR printable chars: user input requested here
-    var N_CHAR = 12
-    var answer = false
-    while !answer do {
-      N_CHAR = 12
-      print(f"\n\nPassword of $N_CHAR%s printable chars OK? 'y' or another integer number >= 8: ")
-
-      val answer_str = scala.io.StdIn.readLine()
-      if answer_str == "y" then
-        answer = true
-      else
-        try
-          N_CHAR = answer_str.toInt
-          if N_CHAR < 8 then
-            print("enter an integer number >= 8 or 'y'")
-          else
-            answer = true
-
-        catch
-          // Exception in thread "main" java.lang.NumberFormatException: For input string: "sdaf"
-          case ex: NumberFormatException =>
-            print("enter an integer number >= 8 or 'y'")
-    }
-
-    var WITH_SPECIAL_CHARS = true
-    answer = false
-    while !answer do {
-      print(f"\nDo you want me to use special characters like .;,+*... ? 'y' or 'n': ")
-
-      val answer_str = scala.io.StdIn.readLine()
-      if answer_str == "y" then
-        answer = true
-      else
-        WITH_SPECIAL_CHARS = false
-        answer = true
-    }
-
-    var char_set = (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')).toSet
-    // https://www.baeldung.com/scala/string-character-check-alphanumeric
-    if WITH_SPECIAL_CHARS then
-      char_set = (('!' to '~')).toSet
-
-
-    var i = 0  // char counter for the password
-    var j = 0  // char counter for x
-    var pw_chars = ""
-
-    while i < N_CHAR do {
-      // convert an integer number into a string of '0' and '1' characters:
-      var bin0 = "0000000000000000" + x(j).toBinaryString takeRight 16
-      // bin0 could be for example ' 111001001100101'
-      // --> padding needed with leading zeros
-
-      var bin0_0 = bin0.substring(0, 8)   // end index is exclusive
-      var bin0_1 = bin0.substring(8, 16)
-
-      // convert a string of '0' and '1' characters into a character:
-      var char0 = Integer.parseUnsignedInt(bin0_0, 2).toChar
-      var char1 = Integer.parseUnsignedInt(bin0_1, 2).toChar
-
-      if char_set.contains(char0) then
-        pw_chars = pw_chars + char0
-        i += 1
-
-      if char_set.contains(char1) && i < N_CHAR then
-        pw_chars = pw_chars + char1
-        i += 1
-
-      j += 1
-    }
-
-    print(f"\nYour password of $N_CHAR%d characters is: $pw_chars%s\n")
   }
 }
 
-// end of random_bitstring_and_flexible_password_generator.scala
+// end of random_streams_for_perf_stats.scala
